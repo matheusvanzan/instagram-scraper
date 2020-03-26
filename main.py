abab5764f69c6812b11b901961a3d3d9e2cf718d
Matheus Vanzan <vanzan2015@gmail.com>
2020-03-25 23:21:49 -0300

    Add models and removed data handling from main

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-import urllib.request 
-import os
-
+from argparse import ArgumentParser
 from time import sleep
 
-user = 'matheusvanzan'
-# user = 'camoa.telecom'
-# user = 'gizbranco.plataforma'
-
-url = 'https://www.instagram.com/' + user
-directory = 'data/' + user
-
-if not os.path.exists(directory):
-    os.makedirs(directory)
+from models import Profile, Post
 
 
+# parse args
+parser = ArgumentParser()
+parser.add_argument('-u', '--username', dest='username', help='Profile username')
+parser.add_argument('-d', '--debug', dest='debug', default=False, required=False, help='If True it shows debug output')
+args = parser.parse_args()
+
+# chrome config
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--window-size=1920x1080')
@@ -26,98 +23,96 @@ chrome = Chrome(
     executable_path=driver
 )
 
-
-chrome.get(url)
-
-print('Chrome running at ', chrome.current_url)
-
-
-chrome.get_screenshot_as_file('screen.png')
-
+chrome.get('https://www.instagram.com/' + args.username)
+if args.debug: print('Chrome running at ', chrome.current_url)
 
 selectors = {
-    'nome': 'header h1',
-    'foto_perfil': 'header img',
+    'name': 'header h1',
     'num_posts': 'header ul li:nth-child(1) span',
-    'num_seguidores': 'header ul li:nth-child(2) span',
-    'num_seguindo': 'header ul li:nth-child(3) span',
+    'num_followers': 'header ul li:nth-child(2) span',
+    'num_following': 'header ul li:nth-child(3) span',
     
-    'fotos': {
-        'a': 'main article a',
-        'img': 'main article a img'
-    }
+    'posts': 'main article a',
+    'desc': 'article ul li[role=menuitem] span',
+    'img': 'main article > div img',
+    'local': 'header a',
+    'lat': 'meta[property="place:location:latitude"]',
+    'lng': 'meta[property="place:location:longitude"]',
 }
 
-values = {}
 
-with open('{}/data.csv'.format(directory), 'w+') as f:
-    for key in selectors:
-        if isinstance(selectors[key], str):
-            value = chrome.find_element_by_css_selector(selectors[key])
-            values.update({ key: value.text })
-            f.write('"{}"; "{}"\n'.format(key, value.text))
+# Create Profile instance and get info
+profile = Profile(args.username)
 
+name_el = chrome.find_element_by_css_selector(selectors['name'])
+profile.name = name_el.text
 
-# Scroll ate que todas as fotos estejam visiveis
+num_posts_el = chrome.find_element_by_css_selector(selectors['num_posts'])
+profile.num_posts = int(num_posts_el.text.replace(',', ''))
+
+num_followers_el = chrome.find_element_by_css_selector(selectors['num_followers'])
+profile.num_followers = int(num_followers_el.text.replace(',', '').replace('mil', '').replace('milhões', ''))
+
+num_following_el = chrome.find_element_by_css_selector(selectors['num_following'])
+profile.num_following = int(num_following_el.text.replace(',', ''))
+
+if args.debug: print('Saved', profile)
+profile.save()
+    
+
+# Scroll untill all pictures are visible
 urls = set()
-srcs = set()
+while len(urls) < profile.num_posts:
     
-while int(values['num_posts']) > len(urls):
+    if args.debug: print('Scroll down... ', end='')
     chrome.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    print('Scroll down... ', end='')
+    sleep(1)
     
-    chrome.get_screenshot_as_file('scroll.png')
+    for a in chrome.find_elements_by_css_selector( selectors['posts'] ):
+        urls.add(a.get_attribute('href'))
     
-    urls_list = [a.get_attribute('href') for a in chrome.find_elements_by_css_selector( selectors['fotos']['a'] )]
-    srcs_list = [img.get_attribute('src') for img in chrome.find_elements_by_css_selector( selectors['fotos']['img'] )]
+    if args.debug: print('found', len(urls), 'of', profile.num_posts)
+
+
+for i, url in enumerate(urls):
     
-    [urls.add(url) for url in urls_list]
-    [srcs.add(src) for src in srcs_list]
+    # Create new Post object
+    post = Post(profile)
+    post.id_ = i
+    post.url = url
     
-    print('Found', len(urls), 'of', values['num_posts'])
-
-
-# Descricao da foto
-with open('{}/posts.csv'.format(directory), 'w+') as f:
-    for i, url in enumerate(urls):
-        lat, lng, desc = 0, 0, ''
-        
-        chrome.get(url)
-        chrome.get_screenshot_as_file('post.png')
-        
-        desc_span = chrome.find_elements_by_css_selector('article ul li[role=menuitem] span')
-        
-        if len(desc_span) > 1:
-            desc = desc_span[1].text.encode('utf-8').decode('latin-1') 
-            # TODO: explain https://github.com/gevent/gevent/issues/614
-            #       replace \n by space
-        
-        local = chrome.find_elements_by_css_selector('header a')
-        if len(local) > 0:
-            href = local[-1].get_attribute('href')
-        
-            if 'locations' in href:
-                chrome.get(href)
-                
-                lat_metas = chrome.find_elements_by_css_selector('meta[property="place:location:latitude"]')
-                if len(lat_metas) > 0:
-                    lat = lat_metas[0].get_attribute('content')
-                
-                lng_metas = chrome.find_elements_by_css_selector('meta[property="place:location:longitude"]')
-                if len(lng_metas) > 0:
-                    lng = lng_metas[0].get_attribute('content')
-        
-        line = '"{}"; "{}"; "{}"; "{}"\n'.format(i, lat, lng, desc)
-        print(line)
-        f.write(line)
+    # Navigate to post url
+    chrome.get(url)
+    chrome.get_screenshot_as_file('post.png')
     
-
-# Download das fotos
-for i, src in enumerate(srcs):
-    filename = '{}/{}.png'.format(directory, i)
-    urllib.request.urlretrieve(src, filename)
-
-
-
+    # get description (first comment)
+    desc_el = chrome.find_elements_by_css_selector(selectors['desc'])
+    if len(desc_el) > 1:
+        post.desc = desc_el[1].text.replace('\n', '').encode('utf-8').decode('latin-1')
+        # TODO: explain https://github.com/gevent/gevent/issues/614
+        #       replace \n by space
+        
+    # get image
+    img_el = chrome.find_element_by_css_selector(selectors['img'])
+    post.download_img(img_el.get_attribute('src'))
+    
+    # get locations
+    local_el = chrome.find_elements_by_css_selector(selectors['local'])
+    if len(local_el) > 0:
+        href = local_el[-1].get_attribute('href')
+    
+        if 'locations' in href:
+            chrome.get(href)
+            
+            lat_metas = chrome.find_elements_by_css_selector(selectors['lat'])
+            if len(lat_metas) > 0:
+                lat = lat_metas[0].get_attribute('content')
+            
+            lng_metas = chrome.find_elements_by_css_selector(selectors['lng'])
+            if len(lng_metas) > 0:
+                lng = lng_metas[0].get_attribute('content')
+    
+    if args.debug: print('Saved', post)
+    post.save()
 
 chrome.quit()
--- models.py
+++ models.py
@@ -1,15 +1,59 @@
+import urllib.request
+import os
 
 
-
-@dataclass
 class Profile:
-    name: str
-    unit_price: float
-    quantity_on_hand: int = 0
     
-@dataclass
+    def __init__(self, username):
+        self.username = username
+        
+        self.name = ''
+        self.num_posts = 0
+        self.num_followers = 0
+        self.num_following = 0
+        
+        self.directory = 'data/' + username
+        
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+            
+        open('{}/posts.csv'.format(self.directory), 'w+')
+            
+    def __repr__(self):
+        return '<Profile {}>'.format(self.username)
+        
+    def save(self):
+        
+        with open('{}/data.csv'.format(self.directory), 'w+') as f:
+            
+            f.write('"{}"; "{}"\n'.format('name',          self.name))
+            f.write('"{}"; "{}"\n'.format('num_posts',     self.num_posts))
+            f.write('"{}"; "{}"\n'.format('num_followers', self.num_followers))
+            f.write('"{}"; "{}"\n'.format('num_following', self.num_following))
+    
+    
 class Post:
-    name: str
-    unit_price: float
-    quantity_on_hand: int = 0
     
+    def __init__(self, profile):
+        self.profile = profile
+        self.id_ = 0
+        self.url = ''
+        
+        self.lat = 0
+        self.lng = 0
+        self.desc = ''
+        
+    def __repr__(self):
+        lenght = max(len(self.desc), 30)
+        return '<Post {} {}...>'.format(self.id_, self.desc[:lenght])
+        
+    def download_img(self, src):
+        filename = '{}/{}.png'.format(self.profile.directory, self.id_)
+        urllib.request.urlretrieve(src, filename)
+        
+    def save(self):
+        
+        with open('{}/posts.csv'.format(self.profile.directory), 'a') as f:
+            
+            f.write('"{}"; "{}"; "{}"; "{}"\n'.format(
+                self.id_, self.lat, self.lng, self.desc))
